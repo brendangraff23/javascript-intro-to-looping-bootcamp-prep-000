11979f2d1e19791838345c976d28c7e11c30886c
Brendan <brendangraff@gmail.com>
2018-03-16 20:03:10 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,10 +4,10 @@ function forLoop(array)
   {
     if( i === 1 )
     {
-      array[i] = 'I am 1 strange loop';
+      array[i] = `I am 1 strange loop`;
     } else
     {
-      array[i] = "I am + ${i} + strange loops";
+      array[i] = `I am ${i} strange loops`;
     }
   }
 }
