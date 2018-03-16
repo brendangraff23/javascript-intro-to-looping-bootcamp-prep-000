226e3fb2e44af2aca9e504f254b1cafe45330425
Brendan <brendangraff@gmail.com>
2018-03-16 20:22:12 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,6 +21,9 @@ function whileLoop(number) {
   }
   return 'done';
 }
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
 
 function doWhileLoop(array)
 {
